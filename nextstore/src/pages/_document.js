482e0e066cdbac115fa57c2f0105d4ce633791d0
jesusRamon <jesus.ramon2192@gmail.com>
2023-02-23 19:47:24 -0600

    Clase 15 Google Analytics con Next.js

--- nextstore/src/pages/_document.js
+++ nextstore/src/pages/_document.js
@@ -5,6 +5,15 @@ export default function Document() {
     <Html>
       <Head>
         <title>React Shop</title>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-NVJBPKELB9"></script>
+        <script dangerouslySetInnerHTML={{
+          __html:`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-NVJBPKELB9');
+            `
+        }}></script>
       </Head>
       <body>
         <Main />
